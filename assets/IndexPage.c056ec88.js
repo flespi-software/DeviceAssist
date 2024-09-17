75cfdac501a48e19fb51fa743c42b2411152fc8b
Evgenij Spitsyn <spev@gurtam.com>
2024-09-17 14:29:31 +0300

    publish: Initial commit
    generated from commit 594f1d30a36f16c46003d2c8d29af68a81a88984

---
+++ assets/IndexPage.c056ec88.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.1c336079.js";import{_ as a,d as o,B as r,C as n}from"./index.0f1a3349.js";import"./render.56e7dc30.js";const t=o({name:"IndexPage"});function s(c,p,f,_,d,m){return r(),n(e,{class:"flex flex-center"})}var u=a(t,[["render",s]]);export{u as default};
